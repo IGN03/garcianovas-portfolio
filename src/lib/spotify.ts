6b578310f4bb3314c0871ba4ef5dacab0eca7bb7
Ignacio Garcia <ignaciogn2003@gmail.com>
2026-03-30 00:59:02 -0400

    replaced web graph with recent 5

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -10,6 +10,8 @@ const basic = btoa(`${client_id}:${client_secret}`);
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
 const TOP_ARTISTS_ENDPOINT = `https://api.spotify.com/v1/me/top/artists`;
+const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`;
+const RECENTLY_PLAYED_ENDPOINT = `https://api.spotify.com/v1/me/player/recently-played`;
 
 // 1. Get Temporary Access Token
 const getAccessToken = async () => {
@@ -40,10 +42,32 @@ export const getNowPlaying = async () => {
 };
 
 // 3. Fetch Top Artists Data (Used for the charts)
-export const getTopArtists = async () => {
+export const getTopArtists = async (time_range = 'short_term') => {
   const { access_token } = await getAccessToken();
 
-  return fetch(`${TOP_ARTISTS_ENDPOINT}?time_range=short_term&limit=50`, {
+  return fetch(`${TOP_ARTISTS_ENDPOINT}?time_range=${time_range}&limit=50`, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+};
+
+// 4. Fetch Top Tracks Data
+export const getTopTracks = async (time_range = 'short_term') => {
+  const { access_token } = await getAccessToken();
+
+  return fetch(`${TOP_TRACKS_ENDPOINT}?time_range=${time_range}&limit=10`, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+};
+
+// 5. Fetch Recently Played Tracks
+export const getRecentlyPlayed = async (limit = 1) => {
+  const { access_token } = await getAccessToken();
+
+  return fetch(`${RECENTLY_PLAYED_ENDPOINT}?limit=${limit}`, {
     headers: {
       Authorization: `Bearer ${access_token}`,
     },
